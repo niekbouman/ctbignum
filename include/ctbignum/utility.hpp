81df4d19ef88c9f6ff4e3798436426552a3a639d
Niek J. Bouman <niekbouman@gmail.com>
2018-02-27 16:31:00 +0100

    minor cleanup of utility.hpp

--- include/ctbignum/utility.hpp
+++ include/ctbignum/utility.hpp
@@ -1,8 +1,9 @@
 #ifndef CT_UTILITY_HPP
 #define CT_UTILITY_HPP
 
+#include <array>
 #include <cstddef>
-#include<utility>
+#include <utility>
 
 namespace cbn {
 namespace detail {
@@ -18,22 +19,20 @@ constexpr auto tight_length(Array<T, N> num) {
   return L;
 }
 
-
 template <typename T, T... Is>
 constexpr auto tight_length(std::integer_sequence<T, Is...>)
 
 {
   // count the effective number of limbs
   // (ignoring zero-limbs at the most-significant-limb side)
-  
+
   size_t L = sizeof...(Is);
-  std::array<T, sizeof...(Is)> num { Is... };
+  std::array<T, sizeof...(Is)> num{Is...};
   while (L > 0 && num[L - 1] == 0)
     --L;
 
   return L;
 }
-
 
 } // end of detail namespace
 } // end of cbn namespace
