762bacb5ea8d4dbd883942fda6e8b66eed62743d
Niek J. Bouman <niekbouman@gmail.com>
2018-03-18 22:52:22 +0100

    added print.hpp

---
+++ include/ctbignum/print.hpp
@@ -0,0 +1,78 @@
+#ifndef MYPRINT_HPP
+#define MYPRINT_HPP
+
+#include <cppitertools/itertools.hpp>
+#include <iostream>
+#include <sprout/array.hpp>
+#include <ctbignum/bigint.hpp>
+#include <ctbignum/io.hpp>
+
+
+
+template <typename T, size_t N>
+std::ostream &operator<<(std::ostream &os, std::array<T, N> big_int) {
+  os << '{';
+  for (auto &&e : iter::enumerate(big_int)) {
+    os << e.element;
+    if (e.index != big_int.size() - 1)
+      os << ", ";
+  }
+  os << "}";
+  return os;
+}
+
+/*
+template <typename T, size_t N>
+std::ostream &operator<<(std::ostream &os, sprout::array<T, N> big_int) {
+  std::array<T, N> tmp;
+
+  auto i = 0;
+  for (auto elem : big_int)
+    tmp[i++] = elem;
+
+  os << tmp;
+  return os;
+}
+*/
+
+template <typename Arg, typename... Args>
+void print(Arg &&arg, Args &&... args) {
+  std::cout << std::forward<Arg>(arg);
+  using expander = int[];
+  (void)expander{0, (void(std::cout << " " << std::forward<Args>(args)), 0)...};
+  std::cout << std::endl;
+}
+//template <typename Arg, typename... Args>
+//void print(Arg &&arg, Args &&... args) {
+//  print_(std::forward<Arg>(arg),std::forward<Args...>(args...));
+//  std::cout << std::endl;
+//}
+
+/*
+template <typename T, size_t N>
+void print(big_int<N, T> big_int)
+{
+  print_('{');
+  for (auto&& e: iter::enumerate(big_int))
+  {
+    print_(e.element);
+    if (e.index != big_int.size()-1)
+      print_(", ");
+  }
+  print_("}\n");
+}
+
+template <typename T, size_t N> void print(sprout::array<T, N> big_int) {
+  std::array<T, N> tmp;
+
+  auto i = 0;
+  for (auto elem : big_int)
+    tmp[i++] = elem;
+
+  print(tmp);
+}
+*/
+
+
+
+#endif
