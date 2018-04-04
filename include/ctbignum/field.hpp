4b97caee8612ac6135b75a7663993ba6c0936445
Niek J. Bouman <niekbouman@gmail.com>
2018-04-04 22:22:05 +0200

    Boost.Hana is no longer a dependency
    * New user defined literal _Z which returns a integer_sequence with
    limbs
    * New function to_big_int with optional length template parameter
    * Boost.Hana (and hence, Boost) is no longer needed as a dependency

--- include/ctbignum/field.hpp
+++ include/ctbignum/field.hpp
@@ -2,12 +2,13 @@
 #define CT_FIELD_HPP
 
 #include <ctbignum/addition.hpp>
-#include <ctbignum/mult.hpp>
 #include <ctbignum/barrett.hpp>
 #include <ctbignum/initialization.hpp>
+#include <ctbignum/mult.hpp>
+#include <ctbignum/slicing.hpp>
 
 #include <cstddef>
-#include <boost/hana/string.hpp>
+//#include <boost/hana/string.hpp>
 #include <type_traits>
 
 namespace cbn {
@@ -22,12 +23,21 @@ template <typename T, T... Modulus> struct ZqElement {
 
   constexpr ZqElement() {}
 
+  template <T... Limbs>
+  constexpr ZqElement(std::integer_sequence<T, Limbs...>)
+      // reduce init mod q if necessary, where q = Modulus
+      : data(big_int<sizeof...(Limbs), T>{Limbs...} <
+                     big_int<sizeof...(Modulus), T>{Modulus...}
+                 ? detail::to_length<sizeof...(Modulus)>(
+                       big_int<sizeof...(Limbs), T>{Limbs...})
+                 : barrett_reduction(big_int<sizeof...(Limbs), T>{Limbs...},
+                                     std::integer_sequence<T, Modulus...>())) {}
 
   template <std::size_t N>
   constexpr ZqElement(big_int<N, T> init)
   // reduce init mod q if necessary, where q = Modulus
       : data(init < big_int<sizeof...(Modulus), T>{Modulus...}
-                 ? detail::first<sizeof...(Modulus)>(init)
+                 ? detail::to_length<sizeof...(Modulus)>(init)
                  : barrett_reduction(init,
                                      std::integer_sequence<T, Modulus...>())) {}
 
@@ -35,6 +45,7 @@ template <typename T, T... Modulus> struct ZqElement {
       : data(init) {}
 
 
+  /*
   template <typename String,
             typename = std::enable_if_t< // only bind to Hana strings
                 boost::hana::is_a<boost::hana::string_tag, String>>>
@@ -42,6 +53,7 @@ template <typename T, T... Modulus> struct ZqElement {
       : data(div(string_to_big_int(s),
                  big_int<sizeof...(Modulus), T>{Modulus...})
                  .second) {} // reduce s mod q, where q = Modulus
+                 */
 };
 
 template <typename T, T... Modulus>  
@@ -50,11 +62,20 @@ auto Zq(std::integer_sequence<T, Modulus...>)
   return ZqElement<T, Modulus...>{};
 }
 
+/*
+template <size_t N, typename T, T... Modulus> 
+constexpr auto Zq(big_int<N, T> modulus) {
+  constexpr auto b = as_integer_seq(modulus);
+  return Zq(b);
+}
+*/
+/*
 template <typename String>
 auto Zq(String s)
 {
   return Zq(cbn::string_to_integer_seq(s));
 }
+*/
 
 template <typename T, T... M>
 constexpr auto operator+(ZqElement<T, M...> a, ZqElement<T, M...> b) {
