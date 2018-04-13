dd0c90ddfcceecf99d193e477872313cd226aff6
niekbouman <niekbouman@gmail.com>
2018-04-13 23:29:35 +0200

    Update bigint.hpp

--- include/ctbignum/bigint.hpp
+++ include/ctbignum/bigint.hpp
@@ -6,14 +6,6 @@
 // C++ Library for Compile-Time and Run-Time Multi-Precision and Modular Arithmetic
 // 
 //
-//
-// This file is part of
-//
-// CTBignum 	
-//
-// C++ Library for Compile-Time and Run-Time Multi-Precision and Modular Arithmetic
-// 
-//
 // This file is distributed under the Apache License, Version 2.0. See the LICENSE
 // file for details.
 #ifndef CT_BIGINT_HPP
