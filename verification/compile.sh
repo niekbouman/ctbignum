7885d7f928a4318a58a3f718b2aa20950e9e8f84
Niek J. Bouman <niekbouman@gmail.com>
2018-08-03 11:44:42 +0200

    refactored cryptol files and added multiplication test
    - addition verification works well (fast)
    - not sure about multiplication: verification is running for several
      hours now and has not yet ended

--- verification/compile.sh
+++ verification/compile.sh
@@ -1,10 +1,16 @@
 LLVM_SUFFIX="-mp-6.0"
 
+if [ "$#" -ne 2 ]; then
+  echo "Use: $0 <filename (without extension)> <function name>"
+  exit 1
+fi
+
 echo 'Compiling...'
-eval "clang++$LLVM_SUFFIX -std=c++17 -c -emit-llvm -o add.bc add.cpp" && \
+eval "clang++$LLVM_SUFFIX -I../include -std=c++17 -c -emit-llvm -o $1.bc $1.cpp" && \
 echo 'done.' && \
 echo && \
 echo 'Mangled function name has become:' && \
-eval "llvm-dis$LLVM_SUFFIX -o - add.bc | python3 extract_mangled_name.py add"
+eval "llvm-dis$LLVM_SUFFIX -o - $1.bc | python3 extract_mangled_name.py $2" && \
+exit 0
 
 
---
+++ verification/mul.cpp
@@ -0,0 +1,4 @@
+#include <ctbignum/bigint.hpp>
+#include <ctbignum/mult.hpp>
+
+template auto cbn::mul(big_int<3, uint64_t>, big_int<4, uint64_t>);
