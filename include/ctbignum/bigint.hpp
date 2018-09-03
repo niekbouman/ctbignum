32600c63ae6352748a4d2d7fd0d5f6701604c212
Niek J. Bouman <niekbouman@gmail.com>
2018-09-03 18:59:00 +0200

    simplified definition of big_int
    Now inherits entire interface from std::array

--- include/ctbignum/bigint.hpp
+++ include/ctbignum/bigint.hpp
@@ -17,13 +17,8 @@ namespace cbn {
 
 template <size_t N, typename T = uint64_t,
           typename = std::enable_if_t<std::is_integral<T>::value>>
-struct big_int {
-  std::array<T, N> repr;
+struct big_int : std::array<T, N> {};
 
-  constexpr T &operator[](size_t i) { return repr[i]; }
-  constexpr const T &operator[](size_t i) const { return repr[i]; }
-  constexpr size_t size() const noexcept { return repr.size(); }
-};
 }
 
 #endif
