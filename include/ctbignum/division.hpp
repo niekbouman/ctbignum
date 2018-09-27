217e733de1bedf60b13132933c51a1927b240a93
Niek J. Bouman <niekbouman@gmail.com>
2018-09-27 08:36:04 -0700

    cleanup in division function

--- include/ctbignum/division.hpp
+++ include/ctbignum/division.hpp
@@ -23,11 +23,9 @@
 
 namespace cbn {
 
-template <typename Q, typename R>
-struct DivisionResult
-{
+template <typename Q, typename R> struct DivisionResult {
   Q quotient;
-  R remainder;  
+  R remainder;
 };
 
 template <size_t M, typename T> constexpr  
@@ -44,16 +42,16 @@ short_div(big_int<M, T> u, T v) {
   return {q, {static_cast<T>(r)}};
 }
 
-
-template <size_t NN, size_t NplusM, typename T>
-constexpr DivisionResult<big_int<NplusM,T>, big_int<NN, T>>  div(big_int<NplusM, T> u, big_int<NN, T> v) {
+template <size_t M, size_t N, typename T>
+constexpr DivisionResult<big_int<M, T>, big_int<N, T>> div(big_int<M, T> u,
+                                                           big_int<N, T> v) {
   // Knuth's "Algorithm D" for multiprecision division as described in TAOCP
   // Volume 2: Seminumerical Algorithms
   // combined with short division
 
   //
   // input:
-  // u  big_int<N+M>
+  // u  big_int<M>,      M>=N
   // v  big_int<N>
   //
   // computes:
@@ -64,68 +62,62 @@ constexpr DivisionResult<big_int<NplusM,T>, big_int<NN, T>>  div(big_int<NplusM,
   // std::pair<big_int<N+M>, big_int<N>>(quotient, rem)
 
   using TT = typename dbl_bitlen<T>::type;
-  size_t N = NN;
-  while (N > 0 && v[N - 1] == 0)
-    --N;
+  size_t tight_N = N;
+  while (tight_N > 0 && v[tight_N - 1] == 0)
+    --tight_N;
 
-  big_int<NplusM, T> q{};
+  big_int<M, T> q{};
 
-  if (N == 1) { // short division
+  if (tight_N == 1) { // short division
     TT r {};
-    for (int i = NplusM - 1; i >= 0; --i) {
+    for (int i = M - 1; i >= 0; --i) {
       TT w = (r << std::numeric_limits<T>::digits) + u[i];
       q[i] = w / v[0];
       r = w % v[0];
     }
-    //return std::make_pair(q, big_int<NN, T>{{static_cast<T>(r)}});
-    //return DivisionResult<decltype(q), big_int<NN, T>>{q, {static_cast<T>(r)}};
     return {q, {static_cast<T>(r)}};
   }
 
   uint8_t k = 0;
-  while (v[N - 1] <
+  while (v[tight_N - 1] <
          (static_cast<T>(1) << (std::numeric_limits<T>::digits - 1))) {
     ++k;
-    v = detail::first<NN>(shift_left(v, 1));
+    v = detail::first<N>(shift_left(v, 1));
   }
   auto us = shift_left(u, k);
 
-  size_t M = NplusM - N;
-  for (int j = M; j >= 0; --j) {
-    TT tmp = us[j + N - 1];
-    TT tmp2 = us[j + N];
+  for (int j = M - tight_N; j >= 0; --j) {
+    TT tmp = us[j + tight_N - 1];
+    TT tmp2 = us[j + tight_N];
     tmp += (tmp2 << std::numeric_limits<T>::digits);
-    TT qhat = tmp / v[N - 1];
-    TT rhat = tmp % v[N - 1];
+    TT qhat = tmp / v[tight_N - 1];
+    TT rhat = tmp % v[tight_N - 1];
 
     auto b = static_cast<TT>(1) << std::numeric_limits<T>::digits;
     while (qhat == b ||
-           (qhat * v[N - 2] >
-            (rhat << std::numeric_limits<T>::digits) + us[j + N - 2])) {
+           (qhat * v[tight_N - 2] >
+            (rhat << std::numeric_limits<T>::digits) + us[j + tight_N - 2])) {
       qhat -= 1;
-      rhat += v[N - 1];
+      rhat += v[tight_N - 1];
       if (rhat >= b)
         break;
     }
-    auto true_value = subtract(detail::take<NN + 1>(us, j, j + N + 1),
+    auto true_value = subtract(detail::take<N + 1>(us, j, j + tight_N + 1),
                                mul(v, big_int<1, T>{{static_cast<T>(qhat)}}));
-    if (true_value[N]) {
+    if (true_value[tight_N]) {
       auto corrected =
-          add_ignore_carry(true_value, detail::unary_encoding<NN + 2>(N + 1));
+          add_ignore_carry(true_value, detail::unary_encoding<N + 2>(tight_N + 1));
       auto new_us_part = add_ignore_carry(corrected, detail::pad<2>(v));
-      for (auto i = 0; i <= N; ++i)
+      for (auto i = 0; i <= tight_N; ++i)
         us[j + i] = new_us_part[i];
       --qhat;
     } else {
-      for (auto i = 0; i <= N; ++i)
+      for (auto i = 0; i <= tight_N; ++i)
         us[j + i] = true_value[i];
     }
     q[j] = qhat;
   }
-  //return std::make_pair(q, shift_right(detail::first<NN>(us), k));
-
-  //return DivisionResult<decltype(q), big_int<NN, T>>{q, shift_right(detail::first<NN>(us), k) };
-  return {q, shift_right(detail::first<NN>(us), k) };
+  return {q, shift_right(detail::first<N>(us), k) };
 }
 
 template <typename T, size_t N1, size_t N2>
