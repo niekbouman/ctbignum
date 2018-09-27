78895ef93a6e868df30fcb821efdaaa7ab3d3dc4
Niek J. Bouman <niekbouman@gmail.com>
2018-09-27 08:37:20 -0700

    field now uses invariant_div instead of barrett reduction

--- include/ctbignum/field.hpp
+++ include/ctbignum/field.hpp
@@ -12,28 +12,22 @@
 #define CT_FIELD_HPP
 
 #include <ctbignum/addition.hpp>
-#include <ctbignum/barrett.hpp>
-#include <ctbignum/decimal_literals.hpp>
+#include <ctbignum/invariant_div.hpp>
 #include <ctbignum/io.hpp>
 #include <ctbignum/mult.hpp>
 #include <ctbignum/slicing.hpp>
 
 #include <cstddef>
-//#include <boost/hana/string.hpp>
 #include <type_traits>
 
 namespace cbn {
 
-class skip_reduction {}; 
+class skip_reduction {};
 
 template <typename T, T... Modulus> struct ZqElement {
-
   big_int<sizeof...(Modulus), T> data;
-
   explicit operator auto() const { return data; } // allow casting to big_int
-
   constexpr ZqElement() : data() {}
-
   constexpr ZqElement(size_t x) : data{x} {}
 
   template <T... Limbs>
@@ -43,30 +37,18 @@ template <typename T, T... Modulus> struct ZqElement {
                      big_int<sizeof...(Modulus), T>{Modulus...}
                  ? detail::to_length<sizeof...(Modulus)>(
                        big_int<sizeof...(Limbs), T>{Limbs...})
-                 : barrett_reduction(big_int<sizeof...(Limbs), T>{Limbs...},
-                                     std::integer_sequence<T, Modulus...>())) {}
+                 : mod(big_int<sizeof...(Limbs), T>{Limbs...},
+                       std::integer_sequence<T, Modulus...>())) {}
 
   template <std::size_t N>
   constexpr ZqElement(big_int<N, T> init)
-  // reduce init mod q if necessary, where q = Modulus
+      // reduce init mod q if necessary, where q = Modulus
       : data(init < big_int<sizeof...(Modulus), T>{Modulus...}
                  ? detail::to_length<sizeof...(Modulus)>(init)
-                 : barrett_reduction(init,
-                                     std::integer_sequence<T, Modulus...>())) {}
+                 : mod(init, std::integer_sequence<T, Modulus...>())) {}
 
   constexpr ZqElement(big_int<sizeof...(Modulus), T> init, skip_reduction)
       : data(init) {}
-
-
-  /*
-  template <typename String,
-            typename = std::enable_if_t< // only bind to Hana strings
-                boost::hana::is_a<boost::hana::string_tag, String>>>
-  constexpr ZqElement(String s)
-      : data(div(string_to_big_int(s),
-                 big_int<sizeof...(Modulus), T>{Modulus...})
-                 .second) {} // reduce s mod q, where q = Modulus
-                 */
 };
 
 template <typename T, T... Modulus>  
@@ -75,21 +57,6 @@ auto Zq(std::integer_sequence<T, Modulus...>)
   return ZqElement<T, Modulus...>{};
 }
 
-/*
-template <size_t N, typename T, T... Modulus> 
-constexpr auto Zq(big_int<N, T> modulus) {
-  constexpr auto b = as_integer_seq(modulus);
-  return Zq(b);
-}
-*/
-/*
-template <typename String>
-auto Zq(String s)
-{
-  return Zq(cbn::string_to_integer_seq(s));
-}
-*/
-
 template <typename T, T... M>
 constexpr auto operator+(ZqElement<T, M...> a, ZqElement<T, M...> b) {
   return ZqElement<T, M...> { mod_add(a.data, b.data, big_int<sizeof...(M), T>{M...}), skip_reduction{} };
@@ -97,7 +64,7 @@ constexpr auto operator+(ZqElement<T, M...> a, ZqElement<T, M...> b) {
 
 template <typename T, T... M>
 constexpr auto operator*(ZqElement<T, M...> a, ZqElement<T, M...> b) {
-  return ZqElement<T, M...> { barrett_reduction(mul(a.data, b.data), std::integer_sequence<T, M...>()), skip_reduction{} };
+  return ZqElement<T, M...> { mod(mul(a.data, b.data), std::integer_sequence<T, M...>()), skip_reduction{} };
 }
 
 template <typename T, T... M>
