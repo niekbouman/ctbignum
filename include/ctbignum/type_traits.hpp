12239791bca2ea10bbeeb1529349ec7603e66f46
niekbouman <niekbouman@gmail.com>
2018-11-07 08:29:09 +0100

    Reverted update to type_traits
    Specialization for unsigned long was breaking literal parsing.

--- include/ctbignum/type_traits.hpp
+++ include/ctbignum/type_traits.hpp
@@ -20,6 +20,6 @@ template <> struct dbl_bitlen<uint8_t> { using type = uint16_t; };
 template <> struct dbl_bitlen<uint16_t> { using type = uint32_t; };
 template <> struct dbl_bitlen<uint32_t> { using type = uint64_t; };
 template <> struct dbl_bitlen<uint64_t> { using type = __uint128_t; };
-template <> struct dbl_bitlen<unsigned long> { using type = __uint128_t; };
+//template <> struct dbl_bitlen<unsigned long> { using type = __uint128_t; };
 }
 #endif
